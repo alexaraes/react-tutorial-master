9839f85445ccdbc036385fb2e942cd2735ba5657
Alexa Schreffler <alexa.schreffler@gmail.com>
2016-01-25 15:59:19 -0600

    server workin n junk

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,14 +1,29 @@
-var data = [
-	{id: 1, author: "Pete Hunt", text: "This is one comment."},
-	{id: 2, author: "Jordan Walke", text: "This is another comment"}
-];
-
 var CommentBox = React.createClass({
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString())
+			}.bind(this)
+		});
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
 	render: function() {
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
+				<CommentList data={this.state.data} />
 				<CommentForm />
 			</div>
 		);
@@ -17,7 +32,7 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
 	render: function() {
-		var commentNodes = this.props.data.map(function() {
+		var commentNodes = this.props.data.map(function(comment) {
 			return (
 				<Comment author={comment.author} key={comment.id}>
 					{comment.text}
@@ -33,11 +48,42 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+	getInitialState: function() {
+		return {author: '', text: ''};
+	},
+	handleAuthorChange: function(e) {
+		this.setState({author: e.target.value});
+	},
+	handleTextChange: function(e) {
+		this.setState({text: e.target.value});
+	},
+	handleSubmit: function() {
+		e.preventDefault();
+		var author = this.state.author.trim();
+		var text = this.state.text.trim();
+		if(!text || !author) {
+			return;
+		}
+
+		this.setState({author: '', text: ''})
+	},
 	render: function() {
 		return (
-			<div className="commentForm">
-				Hello World! I am a comment form.
-			</div
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input 
+					type="text" 
+					placeholder="Your Name"
+					value={this.state.author}
+					onChange={this.handleAuthorChange}
+				/>
+				<input
+					type="text"
+					placeholder="Say Something"
+					value={this.state.text}
+					onChange={this.handleTextChange}
+				/>
+				<input type="submit" value="Post" />
+			</form>
 		);
 	}
 });
@@ -59,7 +105,7 @@ var Comment = React.createClass({
 	}
 });
 
-ReactDom.render(
-	<CommentBox url="/api/comments" />,
+ReactDOM.render(
+	<CommentBox url="/api/comments" pollInterval={2000} />,
 	document.getElementById('content')
 );
